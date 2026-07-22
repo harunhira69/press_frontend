8ad231ab3f1f008a8167c47614b9cc915180c2c4
HARUN OR RASHID <harunabhi4@gmail.com>
2026-07-22 16:13:50 -0700

    feat(auth): build shadcn login form and manage input state with useState

---
+++ app/(auth)/_components/LoginFrom.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import React, { useActionState, useEffect } from "react";
+import { loginAction } from "../_actions/authAction";
+import { toast } from "sonner";
+
+const LoginFrom = () => {
+    const [state,action,pending] = useActionState(loginAction,false)
+
+   useEffect(()=>{
+    if(!state) return;
+
+  if(state.success){
+      toast.error(state.message || "User login successfully")
+  }
+
+   if(!state.success){
+      toast.error(state.message || "Login Failed")
+  }
+  
+
+   },[state])
+  return (
+    <form action={action} className="space-y-4">
+      <Card className="p-5 space-y-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Enter your Email"
+          required
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter Your password"
+          required
+        />
+
+        <Button>
+           {
+            pending ? "Submitting" : "Login"
+           }
+        </Button>
+      </Card>
+    </form>
+  );
+};
+
+export default LoginFrom;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import { Roboto, EB_Garamond } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
 const ebGaramondHeading = EB_Garamond({subsets:['latin'],variable:'--font-heading'});
 
@@ -19,6 +20,10 @@ export default function RootLayout({
       className={cn("h-full antialiased", "font-sans", roboto.variable, ebGaramondHeading.variable)}
     >
       <body className="min-h-full flex flex-col">
+
+
+
+        <Toaster position="top-right" richColors/>        
         {children}
         </body>
     </html>
